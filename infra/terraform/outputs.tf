051c144f34f3df5566e280536cf0fd1465c9abaa
Sujay Muramalla <37665429+Sujay-Muramalla@users.noreply.github.com>
2026-03-10 22:01:36 +0100

    Feature/geo s2 cloudtrail baseline (#11)
    * fix(bod): restore original branch after syncing develop
    
    * GEO-21: add CloudTrail logging baseline
    
    * GEO-21: add CloudTrail outputs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -25,3 +25,23 @@ output "public_sg_id" {
 output "private_sg_id" {
   value = module.vpc.private_sg_id
 }
+
+output "cloudtrail_name" {
+  description = "Name of the CloudTrail trail"
+  value       = aws_cloudtrail.geo_sentinel_trail.name
+}
+
+output "cloudtrail_bucket" {
+  description = "S3 bucket used for CloudTrail logs"
+  value       = aws_s3_bucket.cloudtrail_logs.bucket
+}
+
+output "cloudtrail_arn" {
+  description = "ARN of the CloudTrail trail"
+  value       = aws_cloudtrail.geo_sentinel_trail.arn
+}
+
+output "cloudtrail_bucket_arn" {
+  description = "ARN of the CloudTrail log bucket"
+  value       = aws_s3_bucket.cloudtrail_logs.arn
+}
